74582d8fef6b586e7d8214c0436ee9532f01c3fa
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-06 14:03:30 -0500

    2nd palindrom passed

--- TDDOne/tddone/src/main/java/rocks/zipcode/StringUtils.java
+++ TDDOne/tddone/src/main/java/rocks/zipcode/StringUtils.java
@@ -12,35 +12,29 @@ package rocks.zipcode;
  // Student must implement this class
 public class StringUtils {
     // TODO: Implement the following methods to make the tests pass
-    
+
     public static String reverse(String str) {
         StringBuilder sb = new StringBuilder(str);
-         sb.reverse();
-    return sb.toString();
+        sb.reverse();
+        return sb.toString();
     }
-    
+
     public static int countOccurrences(String str, char c) {
         int count = 0;
-        for( int i = 0; i<str.length(); i++){
-            if(str.charAt(i)==c) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
                 count++;
             }
 
         }
         return count;
     }
-    
-    public static boolean isPalindrome(String str) {
-        String minusPunc = str.replaceAll(",","" );
-        String minusAll = minusPunc.replaceAll(":","");
-        String paramMinusPunc=str.replaceAll(",","");
-        String paramMinusAll= paramMinusPunc.replaceAll(" ","");
-        StringBuilder sb = new StringBuilder(minusAll);
-        StringBuilder reversed =sb.reverse();
-        if(reversed.equals(str)){
-//            System.out.println( str +" is a palindrome");
-        }    return reversed.toString().toLowerCase().equals(paramMinusAll);
 
+    public static boolean isPalindrome(String str) {
+        String clean = str.replaceAll("[\\W]|_ ", "");
+        String reversedClean = new StringBuilder(clean).reverse().toString();
+//
+        return reversedClean.equalsIgnoreCase(clean);
     }
 }
 
